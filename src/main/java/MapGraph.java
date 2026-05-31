ae5e8456c677b3bebc13053bbe3cf39e86e6a802
Roshani <dhiry012@mymail.unisa.edu.au>
2026-05-31 15:11:05 +0930

    Create the MapGraph class
    - This class is based on the textbook example
    - Change the edges variable to be a Map with String keys and List<Edge> values (rather than the textbook list of Maps), as it seems easier to traverse/search
    - Update methods according to new edges variable type

---
+++ src/main/java/MapGraph.java
@@ -0,0 +1,89 @@
+import java.util.*;
+
+public class MapGraph extends AbstractGraph {
+    private Map<String, List<Edge>> edges;
+
+    /** Contructs a map graph with the specified vertices and directionality.
+     *
+     * @param numV The number of vertices
+     * @param vertices A list of all the vertices in the map
+     * @param directed The directionality flag
+     */
+    public MapGraph(int numV, String[] vertices, boolean directed) {
+        super(numV, vertices, directed);
+        edges = new HashMap<>();
+        for (String vertex : vertices) {
+            edges.put(vertex, new ArrayList<Edge>());
+        }
+    }
+
+    /** Determines whether an edge exists.
+     *
+     * @param source The source vertex
+     * @param dest The destination vertex
+     * @return true if there is an edge from source to dest
+     */
+    public boolean isEdge(String source, String dest) {
+        try {
+            return edges.get(source).contains(new Edge (source, dest));
+        }
+        catch (NullPointerException e) {  // Throws exception if source is not in the map.
+            return false;
+        }
+    }
+
+    /** Inserts an Edge into the map graph.
+     * @param edge The new edge to be inserted
+     */
+    public void insert(Edge edge) {
+        String source = edge.getSource();
+        String dest = edge.getDest();
+
+        if (!edges.containsKey(source)) {   //Add new key-value pair if source is not yet a vertex.
+            edges.put(source, new ArrayList<>());
+        }
+        edges.get(source).add(edge);
+
+        if (!isDirected()) {   //Create edge in reverse direction if the map is not directed.
+            Edge reverseEdge = new Edge(dest, source);
+            edges.get(source).add(reverseEdge);
+        }
+    }
+
+    /** Creates an iterator object for the list of edges of a source vertex
+     * @param source The source vertex
+     * @return Iterator object, or null if the source vertex is not in the map
+     */
+    public Iterator<Edge> edgeIterator(String source) {
+        try {
+            return edges.get(source).iterator();
+        }
+        catch (NullPointerException e) {
+            return null;
+        }
+    }
+
+    /** Returns the Edge object between a given source vertex and destination vertex, if it exists in the map.
+     * @param source The source vertex
+     * @param dest The destination vertex
+     * @return The existing Edge object, or null if the edge does not exist
+     */
+    public Edge getEdge(String source, String dest) {
+        try {
+            List<Edge> edgeList = edges.get(source);
+            int index = edgeList.indexOf(new Edge(source, dest));
+            if (index < 0) {
+                return null;
+            }
+            return edgeList.get(index);
+        }
+        catch (NullPointerException e) {
+            return null;
+        }
+    }
+
+    /** Code for the Graph interface inspired by:
+     * Koffman, E. B. & Wolfgang, P. A. T. (2016). Data structures: Abstraction and design using Java (3rd ed.). Wiley.
+     */
+}
+
